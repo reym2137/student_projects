8ed265cdf6781345fe043f330f117c6b41c0655d
reym2137 <155949708+reym2137@users.noreply.github.com>
2024-04-23 10:20:15 +0200

    Add files via upload

---
+++ python/1.py
@@ -0,0 +1,21 @@
+slownik = {
+    'okno': "window",
+    'jablko': 'apple',
+    'klawiatura': 'keyboard',
+    'czlowiek': 'human',
+    'Wojtek': 'gay',
+}
+slowo=input('podaj słowo po polsku: ')
+if slowo in slownik:
+    print(slownik[slowo])
+else:
+    print('w slowniku nie ma takiego slowa.')
+    x = str(input('Czy chcesz je dodać?(nie/tak)'))
+    if x == 'nie':
+        exit()
+    elif x=='tak':
+            z = str(input('podaj, co znaczy to słowo po angielsku: '))
+            slownik[slowo] = z
+            print(slownik)
+    else:
+        exit
